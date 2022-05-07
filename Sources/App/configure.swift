4157428a19825aa7e816ee23fe00116af78b7e83
Butanediol <iam@butanediol.me>
2022-05-07 11:19:37 +0000

    Enable file middleware

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,7 +8,7 @@ import telegram_vapor_bot
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
