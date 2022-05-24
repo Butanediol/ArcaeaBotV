9ae0261b0c5dc3594dda5079967bde859398e44f
Butanediol <n872485238@gmail.com>
2022-05-24 18:15:07 +0800

    Change song title font, fix overflow error

--- Sources/App/Utils/Renderers/Best30ImageRenderer.swift
+++ Sources/App/Utils/Renderers/Best30ImageRenderer.swift
@@ -11,15 +11,27 @@ extension Image {
     }
 }
 
+extension Color {
+    static let pmGreen = Color(red: 199 / 255, green: 248 / 255, blue: 78 / 255, alpha: 1)
+    static let frBlue = Color(red: 71 / 255, green: 141 / 255, blue: 1, alpha: 1)
+    static let prsGreen = Color(red: 199 / 255, green: 248 / 255, blue: 78 / 255, alpha: 1)
+    static let ftrPurple = Color(red: 195 / 255, green: 133 / 255, blue: 244 / 255, alpha: 1)
+    static let bydRed = Color(red: 255 / 255, green: 130 / 255, blue: 136 / 255, alpha: 1)
+}
+
 class Best30ImageRenderer {
     let font: String
+    let titleFont: String
 
     init() throws {
-        guard let font = Bundle.module.path(forResource: "Exo-Regular", ofType: "ttf")
+        guard
+            let font = Bundle.module.path(forResource: "Exo-Regular", ofType: "ttf"),
+            let titleFont = Bundle.module.path(forResource: "NotoSansCJKsc-Regular", ofType: "otf")
         else {
             throw Error.invalidImage(reason: "Failed to initialize renderer due to invalid font file.")
         }
         self.font = font
+        self.titleFont = titleFont
     }
 
     func render(_ best30: StoredBest30, songs: [Song]) throws -> Image {
@@ -35,7 +47,7 @@ class Best30ImageRenderer {
 
             guard let song = songs.first(where: { $0.sid == best30.plays[index].songID }) else { continue }
 
-            let card = try renderCard(play: best30.plays[index], song: song, index: index, scale: scale)
+            let card = try renderCard(play: best30.plays[index], song: song, index: index + 1, scale: scale)
 
             image.draw(
                 image: card,
@@ -51,11 +63,12 @@ class Best30ImageRenderer {
         let indicatorPoint = Point(x: 0, y: 115 * scale)
         let indexPoint = Point(x: -10 * scale, y: 40 * scale)
         let indexFontSize = Double(48 * scale)
-        let songTitlePoint = Point(x: 10 * scale, y: (10 + 18) * scale)
         let titleFontSize = Double(18 * scale)
-        let noteCountPoint = Point(x: 10 * scale, y: (68 + 10) * scale)
         let bodyFontSize = Double(10 * scale)
-        let scorePoint = Point(x: 10 * scale, y: (81 + 18) * scale)
+        let songTitlePoint = Point(x: 10 * scale, y: 10 * scale + Int(titleFontSize))
+        let datePoint = Point(x: 10 * scale, y: 32 * scale + Int(bodyFontSize))
+        let noteCountPoint = Point(x: 10 * scale, y: 68 * scale + Int(bodyFontSize))
+        let scorePoint = Point(x: 10 * scale, y: 81 * scale + Int(titleFontSize))
 
         guard let image = Image(width: canvasSize.width, height: canvasSize.height)
         else { throw Error.invalidImage(reason: "Failed to initialize image.") }
@@ -69,8 +82,8 @@ class Best30ImageRenderer {
         var indicatorColor: Color {
             if play.lostCount == 0 {
                 if play.farCount == 0 {
-                    return .init(red: 199 / 255, green: 248 / 255, blue: 78 / 255, alpha: 1)
-                } else { return .init(red: 71 / 255, green: 141 / 255, blue: 1, alpha: 1) }
+                    return .pmGreen
+                } else { return .frBlue }
             } else {
                 return .white
             }
@@ -91,12 +104,35 @@ class Best30ImageRenderer {
         )
 
         // Song title
+        let title = song.nameJp != "" ? song.nameJp ?? "Nil" : song.nameEn
+        var titleColor: Color {
+            switch play.difficulty {
+            case .past:
+                return .white
+            case .present:
+                return .prsGreen
+            case .future:
+                return .ftrPurple
+            case .beyond:
+                return .bydRed
+            }
+        }
         image.renderText(
-            song.nameEn,
+            title,
             from: songTitlePoint,
-            fontList: [font],
-            color: .white,
+            fontList: [titleFont],
+            color: titleColor,
             size: titleFontSize
+        )
+
+        // Date
+        let date = play.relativeTime
+        image.renderText(
+            date,
+            from: datePoint,
+            fontList: [titleFont],
+            color: .white,
+            size: bodyFontSize
         )
 
         // Note count
@@ -118,6 +154,25 @@ class Best30ImageRenderer {
             color: .white,
             size: titleFontSize
         )
+
+        for x in canvasSize.width - 90 * scale ..< canvasSize.width {
+            let ratio = 1 - Double(x - canvasSize.width + 90 * scale) / Double(90 * scale)
+            for y in songTitlePoint.y - Int(titleFontSize) ..< songTitlePoint.y * 2 {
+                var color: Color {
+                    let original = image.get(pixel: Point(x: x, y: y))
+                    return .init(
+                        red: original
+                            .redComponent * ratio,
+                        green: original
+                            .greenComponent * ratio,
+                        blue: original
+                            .blueComponent * ratio,
+                        alpha: 1
+                    )
+                }
+                image.set(pixel: Point(x: x, y: y), to: color)
+            }
+        }
 
         return image
     }
