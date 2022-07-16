4177e2d3222d2dfb9331d83e010efc8a680593fd
Butanediol <n872485238@gmail.com>
2022-07-16 13:47:27 +0800

    Fix sort order

--- Sources/App/BotHandlers/CallbackBotHandlers.swift
+++ Sources/App/BotHandlers/CallbackBotHandlers.swift
@@ -128,7 +128,7 @@ enum CallbackBotHandler {
 
         let allHistory = try StoredPlay.query(on: app.db)
             .filter(\.$arcaeaFriendCode, .equal, relationship.arcaeaFriendCode)
-            .sort(\.$timePlayed).all().wait()
+            .sort(\.$timePlayed, .descending).all().wait()
 
         guard let index = allHistory.firstIndex(where: { $0.id == uuid }) else {
             try bot
